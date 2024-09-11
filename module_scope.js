d020075cdbe9032dcbf0d644f4329ec23e4a0ab0
jxkimathi <judescriva8@gmail.com>
2024-09-11 18:53:46 +0300

    Local module scope and module wrappers

---
+++ module_scope.js
@@ -0,0 +1,10 @@
+// Scope of local modules
+(function (message) {
+    const superHero = "Batman";
+    console.log(message, superHero);
+}) ("Hello");
+
+(function (message) {
+    const superHero = "Superman";
+    console.log(message, superHero);
+}) ("Hey");
